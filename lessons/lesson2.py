adf05e8cb976549a6a230554772a1f1ce238a7ea
nura0779 <nurguls1510@gmai.com>
2026-05-08 06:54:24 +0600

    lesson1 added

---
+++ lessons/lesson2.py
@@ -0,0 +1,9 @@
+     class Hero:
+
+         def __init__(self,name, hp, lvl):
+             self.name = name
+             self.hp = hp
+             self.lvl = lvl
+
+         def action(self):
+              print(f"{self.name} base action") 
